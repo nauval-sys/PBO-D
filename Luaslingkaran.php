cc7ca910d0a55ae9119151f4de22d44b513fe0b6
Nauval_RA <if24.nauvalarrahman@mhs.ubpkarawang.ac.id>
2025-10-25 10:11:08 +0700

    Update Luaslingkaran.php

--- Luaslingkaran.php
+++ Luaslingkaran.php
@@ -1,30 +1,13 @@
 <?php
-
-class Luaslingkaran {
+class luaslingkaran{
 
     public const phi = 3.14;
     public int $jari;
-
-    public function __construct($isijari = 1){
-        $this->jari = $isijari;
-    }
-
-    public function tampil($nama ='ban') {
-    $rumus = Luaslingkaran::phi * $this->jari * $this->jari;
-    echo"Lingkaran {$nama} hasilnya adalah : {$rumus}";
-    }
-
-    public static function testing () {
-        echo "<br/>";
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-    echo "<br/>";
-     echo "yes king";   
-    }
 }
 
-$lingkaran = new LuasLingkaran(7);
-$lingkaran->tampil('roda');
-LuasLingkaran::testing();
+$lingkaran = new luaslingkaran();
+$lingkaran ->jari =7;
+
+$rumus = luaslingkaran::phi* $lingkaran->jari*$lingkaran->jari;
+
+echo "hasilnya adalah". $rumus;
